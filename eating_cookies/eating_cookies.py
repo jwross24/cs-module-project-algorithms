3e406c9bf76b20f14d030322d3180ac6d14fe342
jwross24 <jwross24@users.noreply.github.com>
2020-05-19 18:59:08 -0400

    dynamic programming solution for eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,14 +1,31 @@
-'''
-Input: an integer
-Returns: an integer
-'''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+
+
+def eating_cookies(n):
+    '''
+    Input: an integer
+    Returns: an integer
+    '''
+    # Your code here
+    if n in (0, 1):
+        return 1
+    if n == 2:
+        return 2
+    dp = [0]*(n+1)
+    dp[0] = 1
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n + 1):
+        # This is just the Tribonacci sequence!
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+    return dp[n]
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 10
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
