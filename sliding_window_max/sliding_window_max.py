af34c9be5974edce0c0c1cc09680fad29305185c
jwross24 <jwross24@users.noreply.github.com>
2020-05-19 18:34:20 -0400

    complete sliding_window_max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,16 +1,39 @@
-'''
-Input: a List of integers as well as an integer `k` representing the size of the sliding window
-Returns: a List of integers
-'''
-def sliding_window_max(nums, k):
-    # Your code here
+from collections import deque
 
-    pass
+
+def sliding_window_max(nums, k):
+    '''
+    Input: a List of integers as well as an integer `k` representing the size of the sliding window
+    Returns: a List of integers
+    '''
+    # Your code here
+    deq = deque()
+    output = []
+
+    for i, num in enumerate(nums):
+        # Pop from the end indices of smaller elements
+        while deq and nums[deq[-1]] < num:
+            deq.pop()
+
+        # Append the current index
+        deq.append(i)
+
+        # Pop from the front the (i - k)th index, if still in the queue
+        # This means that it's no longer in the sliding window
+        if deq[0] == i - k:
+            deq.popleft()
+
+        # Once the window is size k, append the current window maximum to output
+        if i + 1 >= k:
+            output.append(nums[deq[0]])
+
+    return output
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
