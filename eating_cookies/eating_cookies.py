d1b73ea49835bd854aabc85278c0b99d9dbb47f0
jwross24 <jwross24@users.noreply.github.com>
2020-05-20 13:35:17 -0400

    add tribonacci matrix code

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,6 +1,3 @@
-
-
-
 def eating_cookies(n):
     '''
     Input: an integer
@@ -23,9 +20,58 @@ def eating_cookies(n):
     return dp[n]
 
 
+# def multiply(A, B):
+#     '''
+#     Input:  A - 3x3 matrix
+#             B - 3x3 matrix
+#     Output: C - 3x3 matrix product of A and B
+#     '''
+#     C = [[0]*3]*3
+#     for i in range(3):
+#         for j in range(3):
+#             total = 0
+#             for k in range(3):
+#                 total = total + A[i][k] * B[k][j]
+#             C[i][j] = total
+#     return C
+
+
+# def power(A, n):
+#     '''
+#     Input:  A - 3x3 matrix
+#             n - exponent of matrix
+#     Output: ret - the 3x3 matrix A^n
+#     '''
+#     ret = [[1, 0, 0],
+#            [0, 1, 0],
+#            [0, 0, 1]]
+
+#     while n > 0:
+#         if n & 1:
+#             ret = multiply(ret, A)
+#         n >>= 1
+#         A = multiply(A, A)
+
+#     return ret
+
+
+# def eating_cookies(n):
+#     '''
+#     Input: an integer
+#     Returns: an integer
+#     '''
+#     # Your code here
+#     Q = [[1, 1, 1],
+#          [1, 0, 0],
+#          [0, 1, 0]]
+
+#     res = power(Q, n)
+#     return res[0][0]
+
+
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 10
+    num_cookies = 5
 
     print(
         f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
