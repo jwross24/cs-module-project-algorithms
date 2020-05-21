a8a2c2c9374de918cb734c8e18c57ba4a0a15504
jwross24 <jwross24@users.noreply.github.com>
2020-05-21 11:32:43 -0400

    use tribonacci sequence approach

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,24 +1,46 @@
+import math
+
+
+# def eating_cookies(n):
+#     '''
+#     Input: an integer
+#     Returns: an integer
+#     '''
+#     # Your code here
+#     if n in (0, 1):
+#         return 1
+#     if n == 2:
+#         return 2
+#     dp = [0]*(n+1)
+#     dp[0] = 1
+#     dp[1] = 1
+#     dp[2] = 2
+
+#     for i in range(3, n + 1):
+#         # This is just the Tribonacci sequence!
+#         dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+#     return dp[n]
+
+
 def eating_cookies(n):
+    # Tribonacci sequence approach
     '''
     Input: an integer
     Returns: an integer
     '''
-    # Your code here
     if n in (0, 1):
         return 1
     if n == 2:
         return 2
-    dp = [0]*(n+1)
-    dp[0] = 1
-    dp[1] = 1
-    dp[2] = 2
+    first = 0
+    second = 1
+    third = 1
+    for _ in range(n - 1):
+        fourth = first + second + third
+        first, second, third = second, third, fourth
 
-    for i in range(3, n + 1):
-        # This is just the Tribonacci sequence!
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
-
-    return dp[n]
-
+    return third
 
 # def multiply(A, B):
 #     '''
@@ -67,7 +89,6 @@ def eating_cookies(n):
 
 #     res = power(Q, n)
 #     return res[0][0]
-
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
